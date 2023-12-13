02d469559b094d11da6c24b04ec7908b15835f34
Arsien Kavalievic <arsienkavalievic@gmail.com>
2023-12-13 17:34:58 +0300

    Add 78. Subsets

---
+++ medium/78.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/subsets/description/
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+
+        for num in nums:
+            result += [subset + [num] for subset in result]
+
+        return result
