294ece1587ee80ab328dfb89e0d8643c984686a9
Arsien Kavalievic <arsienkavalievic@gmail.com>
2023-12-18 21:52:10 +0300

    Add 283. Move Zeroes

---
+++ easy/283.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/move-zeroes/
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        index = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[index] = nums[index], nums[i]
+                index += 1
