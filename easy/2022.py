5662e5830e501d6aa946a1f767e0072ef35b082e
Arsien Kavalievic <arsienkavalievic@gmail.com>
2023-12-18 20:48:40 +0300

    Add 2022. Convert 1D Array Into 2D Array

---
+++ easy/2022.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/convert-1d-array-into-2d-array/
+
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        result = []
+        if m * n == len(original):
+            for i in range(0, len(original), n):
+                result.append(original[i: i + n])
+
+        return result
