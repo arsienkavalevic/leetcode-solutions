cae0421f5ecc31f82ab2215a2de62e60890cfca4
Arsien Kavalievic <arsienkavalievic@gmail.com>
2023-12-28 09:52:35 +0300

    Add 240. Search a 2D Matrix II

---
+++ medium/240.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row, col = 0, len(matrix[0]) - 1
+
+        while row < len(matrix) and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            if matrix[row][col] < target:
+                row += 1
+            else:
+                col -= 1
+        return False
