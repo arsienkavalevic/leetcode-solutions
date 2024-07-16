80023162d9cabc415fac7a827cfb7e61268ab2cd
Arsien Kavalievic <arsienkavalievic@gmail.com>
2024-07-16 18:03:22 +0300

    Add 75. Sort Colors

---
+++ medium/75.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/sort-colors/
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        mid = 0
+        right = len(nums) - 1
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[mid], nums[left] = nums[left], nums[mid]
+                left += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
